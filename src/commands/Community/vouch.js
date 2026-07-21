5adb0bc1c071844a3c8588215c3f9e49fe35b520
imoistaken <ladioretuyi@gmail.com>
2026-07-21 23:42:35 +0100

    Implement vouch command to assign vouch role

---
+++ src/commands/Community/vouch.js
@@ -0,0 +1,43 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+import { successEmbed } from '../../utils/embeds.js';
+import { InteractionHelper } from '../../utils/interactionHelper.js';
+
+const VOUCH_ROLE_ID = "PUT_ROLE_ID_HERE";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName("vouch")
+        .setDescription("Give a user the vouch role.")
+        .addUserOption(option =>
+            option
+                .setName("user")
+                .setDescription("User to vouch")
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
+
+    category: "community",
+
+    async execute(interaction) {
+        const user = interaction.options.getUser("user");
+        const member = await interaction.guild.members.fetch(user.id);
+
+        if (member.roles.cache.has(1529257093234692096)) {
+            return InteractionHelper.universalReply(interaction, {
+                content: "❌ This user already has the vouch role.",
+                ephemeral: true,
+            });
+        }
+
+        await member.roles.add(VOUCH_ROLE_ID);
+
+        await InteractionHelper.universalReply(interaction, {
+            embeds: [
+                successEmbed(
+                    "✅ Vouch Given",
+                    `${user.tag} has been given the vouch role!`
+                ),
+            ],
+        });
+    },
+};
